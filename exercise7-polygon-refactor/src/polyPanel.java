e270a784219e65892148787eaed015bf237e4be7
Chris <chris.r.addington@gmail.com>
2021-10-29 10:55:24 -0400

    Assignment 7
    Refactor began

---
+++ exercise7-polygon-refactor/src/polyPanel.java
@@ -0,0 +1,444 @@
+import java.awt.*;
+import javax.swing.*;
+
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.geom.*;
+import java.util.Stack;
+
+public class polyPanel extends JPanel{
+    // User drawn polygon
+    private Polygon curr_poly;
+
+    // Stack of rectangles for rubberbanding
+    Stack<Polygon> vertex_box;
+
+    // Midpoint algorithm coordinates
+    private int x_cord1;
+    private int x_cord2;
+    private int y_cord1;
+    private int y_cord2;
+
+    // Polygon centroid
+    private int center_x;
+    private int center_y;
+
+    // Polygon attributes
+    private Color poly_color;
+    private int line_width;
+    private boolean filled;
+    private boolean polygon_mode;
+    private boolean bspline_mode;
+    private boolean drag_boxes;
+
+    // Stack to hold the original points
+    private Stack<Point> original_points;
+
+    // Angle of rotation
+    private int angle;
+
+    // Constructor
+    public polyPanel(Polygon given_poly){
+        init(given_poly);
+    }
+
+    public void new_polygon(Polygon given_poly, boolean bspline, boolean fill, boolean draw_drag_box){
+        init(given_poly);
+        bspline_mode = bspline;
+        filled = fill;
+        drag_boxes = draw_drag_box;
+    }
+    public void new_polygon2(Polygon given_poly){
+        init(given_poly);
+    }
+
+    // Initialize the polygon panel
+    public void init(Polygon given_poly){
+        // Make polygon
+        curr_poly = given_poly;
+
+        // Calculate the centroid of the polygon
+        calc_center();
+
+        // Set default attributes
+        poly_color = Color.white;
+        line_width = 3;
+        filled = false;
+        polygon_mode = true;
+        bspline_mode = false;
+        drag_boxes = true;
+
+        // Initialize stack of points
+        original_points = new Stack<Point>();
+
+        // Populate the stack with each point
+        buildStack();
+
+        // Build drag boxes
+        vertex_box = new Stack<Polygon>();
+
+        // Set angle to 0
+        angle = 0;
+    }
+
+    // Calculate the centroid of the polygon
+    public void calc_center(){
+        // Get the x and y coordinate lists
+        int[] x = curr_poly.xpoints;
+        int[] y = curr_poly.ypoints;
+
+        // Accumulators for x and y
+        int total_x = 0;
+        int total_y = 0;
+
+        // Accumulate each point
+        for(int i = 0; i < curr_poly.npoints; i++){
+            total_x += x[i];
+            total_y += y[i];
+        }
+
+        // Create the center point
+        center_x = total_x/curr_poly.npoints;
+        center_y = total_y/curr_poly.npoints;
+
+    }
+
+    // Build the rectangles that allow a user to rubber band a given vertex
+    public void buildDragBoxes(Graphics2D g2d){
+        g2d.setColor(Color.YELLOW);
+        g2d.setStroke(new BasicStroke(1));
+        for(int i = 0; i < curr_poly.npoints; i++) {
+            int x = curr_poly.xpoints[i];
+            int y = curr_poly.ypoints[i];
+            int x2[] = {x-5, x-5, x+5, x+5};
+            int y2[] = {y+5, y-5, y-5, y+5};
+            vertex_box.add(new Polygon(x2, y2, 4));
+            g2d.drawPolygon(vertex_box.elementAt(i));
+        }
+        repaint();
+    }
+
+    // Check if a given vertex is clicked on
+    public int clickInRect(Point click){
+        int vertex_index = -1;
+        if(vertex_box.size() > 0) {
+            for (int i = 0; i < curr_poly.npoints; i++) {
+                if (vertex_box.elementAt(i).contains(click)) {
+                    vertex_index = i;
+                }
+            }
+        }
+        else{
+            System.out.print(vertex_box.size() + "\n");
+        }
+        return vertex_index;
+    }
+
+    // Set operators
+    public void set_color(Color new_color){
+        poly_color = new_color;
+        repaint();
+    }
+    public void set_width(int new_width){
+        line_width = new_width;
+        repaint();
+    }
+    public void set_angle(int new_angle){
+        angle = new_angle;
+        repaint();
+    }
+    public void set_filled(){
+        if(!filled) {
+            filled = true;
+        }
+        else{
+            filled = false;
+        }
+    }
+    public void set_bspline(){
+        if(!bspline_mode){
+            bspline_mode = true;
+        }
+        else{
+            bspline_mode = false;
+        }
+    }
+    public void set_polygon_mode(){
+        if(!polygon_mode){
+            polygon_mode = true;
+        }
+        else{
+            polygon_mode = false;
+        }
+    }
+    public void set_drag_box(boolean set_drag_box){
+        drag_boxes = set_drag_box;
+    }
+
+    // Populate the stack with the original points
+    public void buildStack(){
+        // Get the x and y coordinates
+        int[] x = curr_poly.xpoints;
+        int[] y = curr_poly.ypoints;
+
+        // For each point, create a Point object
+        for(int i = 0; i < curr_poly.npoints; i++){
+            Point curr_point = new Point(x[i], y[i]);
+
+            // Add the current point to the stack
+            original_points.push(curr_point);
+        }
+    }
+
+    // Draw a vertex slightly larger for visibilty
+    public void drawPoints(Graphics2D g){
+        // Get the x and y coordinates
+        int[] x = curr_poly.xpoints;
+        int[] y = curr_poly.ypoints;
+
+        // For each vertex
+        for(int i = 0; i < curr_poly.npoints; i++){
+            // Get circle center points
+            int x_center = x[i] - 3;
+            int y_center = y[i] - 3;
+
+            // Draw an oval around the vertex
+            g.setColor(Color.red);
+            g.drawOval(x_center, y_center, 6, 6);
+        }
+
+        g.setColor(Color.blue);
+        g.drawOval(center_x, center_y, 4, 4);
+    }
+
+    // draw all the lines of the poly using the Midpoint Alg
+    public void draw_poly_by_bres (Graphics2D g) {
+        for (int i=0; i<(curr_poly.npoints-1); i++) {
+            x_cord1 = curr_poly.xpoints[i];
+            x_cord2 = curr_poly.xpoints[i+1];
+            y_cord1 = curr_poly.ypoints[i];
+            y_cord2 = curr_poly.ypoints[i+1];
+            draw_line_by_bres(g);
+        }
+        x_cord1 = curr_poly.xpoints[0];
+        x_cord2 = curr_poly.xpoints[curr_poly.npoints-1];
+        y_cord1 = curr_poly.ypoints[0];
+        y_cord2 = curr_poly.ypoints[curr_poly.npoints-1];
+        draw_line_by_bres(g);
+    }
+
+    // draw a single line segment using Midpoint alg
+    public void draw_line_by_bres (Graphics2D g) {
+
+        float m;
+
+        int dy=Math.abs(y_cord2-y_cord1);
+        int dx=Math.abs(x_cord2-x_cord1);
+        m=(float)dy/(float)dx;
+
+        if(m<=1)
+            slope_less_1(g);
+        else
+            slope_great_1(g);
+    }
+
+
+    //    slope less than 1
+
+    public void slope_less_1 (Graphics g) {
+        int x = x_cord1, y = y_cord1 , p = 0,xEnd=0,yEnd=0;
+        int dx,dy;
+        float m;
+
+        plotpoints(x,y, g);
+        dx = (x_cord1-x_cord2);
+        dy = (y_cord1-y_cord2);
+        m=(float)dy/(float)dx;
+
+        dx = Math.abs(x_cord1-x_cord2);
+        dy = Math.abs(y_cord1-y_cord2);
+        //p = 2 * dy - dx;
+        p = dy - dx;
+        if(x_cord1>x_cord2)
+        {
+            x=x_cord2;
+            y=y_cord2;
+            xEnd = x_cord1;
+        }
+        else
+        {	x=x_cord1;
+            y=y_cord1;
+            xEnd= x_cord2;
+        }
+        plotpoints (x, y, g);
+
+        // Set up the constant "increments"
+        int incrE=2*dy;
+        int incrNE=2*(dy-dx);
+
+        while (x < xEnd) {
+            x++;
+            if (p < 0)
+                p += incrE;
+            else {
+                p += incrNE;
+                if (m < 0) y--;
+                else y++;
+            }
+            plotpoints (x, y, g);
+        }
+    }
+
+    public void slope_great_1 (Graphics g) {
+        int x = x_cord1, y = y_cord1 , p = 0,xEnd=0,yEnd=0;
+        int dx,dy;
+        float m;
+
+        plotpoints(x,y, g);
+        dx = (x_cord1-x_cord2);
+        dy = (y_cord1-y_cord2);
+        m=(float)dy/(float)dx;
+
+        dx = Math.abs(x_cord1-x_cord2);
+        dy = Math.abs(y_cord1-y_cord2);
+        p = 2 * dx - dy;
+        if(y_cord1>y_cord2)
+        {
+            x=x_cord2;
+            y=y_cord2;
+            yEnd = y_cord1;
+        }
+        else
+        {	x=x_cord1;
+            y=y_cord1;
+            yEnd= y_cord2;
+        }
+        plotpoints (x, y, g);
+        int incrE=2*dx;
+        int incrNE=2*(dx-dy);
+        while (y < yEnd)
+        {
+            y++;
+            if (p < 0)
+                p += incrE;
+            else {
+                p += incrNE;
+                if (m <0 ) x--;
+                else x++;
+            }
+            plotpoints (x, y, g);
+        }
+    }
+
+    // Helper method to draw a single point
+    public void  plotpoints(int x, int y, Graphics g) {
+        g.fillRect(x , y , 1,1);
+    }
+
+    public void bspline(Graphics2D g2d){
+        double x, y, xold, yold, A, B, C;
+        double t1, t2, t3;
+        double deltax1, deltax2, deltax3;
+        double deltay1, deltay2, deltay3;
+        int count = curr_poly.npoints;
+        int limit = 4;
+        int intervals = 20;
+        int[] xpoints = curr_poly.xpoints;
+        int[] ypoints = curr_poly.ypoints;
+
+        // must have at least 4 points
+        if (count < limit)
+            return;
+
+        for(int i = 0; i < curr_poly.npoints - 3; i++) {
+            //  This is forward differencing code to draw fast Bspline
+            t1 = 1.0 / intervals;
+            t2 = t1 * t1;
+            t3 = t2 * t1;
+
+            //  For B-spline curve, "D" is the starting x,y coord
+            //  So the first x,y coord is the D term from the cubic equation
+            x = (xpoints[i] + 4.0 * xpoints[i+1] + xpoints[i+2]) / 6.0;
+            y = (ypoints[i] + 4.0 * ypoints[i+1] + ypoints[i+2]) / 6.0;
+            xold = x;
+            yold = y;
+
+            // set up deltas for the x-coords of B-spline
+            A = (-xpoints[i] + 3 * xpoints[i+1] - 3 * xpoints[i+2] + xpoints[i+3]) / 6.0;
+            B = (3 * xpoints[i] - 6 * xpoints[i+1] + 3 * xpoints[i+2]) / 6.0;
+            C = (-3 * xpoints[i] + 3 * xpoints[i+2]) / 6.0;
+
+            deltax1 = A * t3 + B * t2 + C * t1;
+            deltax2 = 6 * A * t3 + 2 * B * t2;
+            deltax3 = 6 * A * t3;
+
+            // set up deltas for the y-coords
+            A = (-ypoints[i] + 3 * ypoints[i+1] - 3 * ypoints[i+2] + ypoints[i+3]) / 6.0;
+            B = (3 * ypoints[i] - 6 * ypoints[i+1] + 3 * ypoints[i+2]) / 6.0;
+            C = (-3 * ypoints[i] + 3 * ypoints[i+2]) / 6.0;
+
+            deltay1 = A * t3 + B * t2 + C * t1;
+            deltay2 = 6 * A * t3 + 2 * B * t2;
+            deltay3 = 6 * A * t3;
+
+            g2d.setStroke(new BasicStroke(3));
+            g2d.setColor(Color.RED);
+
+            for (int j = 0; j < intervals; j++) {
+                x += deltax1;
+                deltax1 += deltax2;
+                deltax2 += deltax3;
+
+                y += deltay1;
+                deltay1 += deltay2;
+                deltay2 += deltay3;
+
+                g2d.drawLine((int) xold, (int) yold, (int) x, (int) y);
+                xold = x;
+                yold = y;
+            }
+
+            g2d.setStroke(new BasicStroke(1));
+            g2d.setColor(poly_color);
+        }
+    }
+
+    // Paint component override
+    public void paintComponent(Graphics g){
+        // Clear drawing area
+        super.paintComponent(g);
+
+        // Set polygon color
+        g.setColor(poly_color);
+
+        // Type cast g to a Graphics2D object and set edge width
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(line_width));
+
+        AffineTransform a = AffineTransform.getRotateInstance (Math.toRadians(angle),
+                (double) center_x, (double) center_y);
+        g2d.setTransform(a);
+        //g2d.rotate(Math.toRadians(angle));
+
+        // Draw the current polygon
+        if(polygon_mode) {
+            draw_poly_by_bres(g2d);
+        }
+        if(bspline_mode){
+            bspline(g2d);
+        }
+
+        // If polygon should be filled, fill it
+        if(filled) {
+            g2d.fill(curr_poly);
+        }
+
+        // Draw the vertices larger for visibility
+        drawPoints(g2d);
+
+        // Build the drag boxes for rubberbanding
+        if(drag_boxes) {
+            buildDragBoxes(g2d);
+        }
+    }
+}
---
+++ exercise7-polygon-refactor/src/polygon_driver.java
@@ -0,0 +1,442 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.Hashtable;
+
+public class polygon_driver extends JFrame implements ActionListener, MouseListener, MouseMotionListener, ItemListener {
+    // GUI elements
+    private JFrame frame;
+    private Container c;
+    private polyPanel polygon_panel;
+    private JPanel draw_panel;
+    private JPanel controls_panel;
+
+    // Buttons to control the application
+    private JButton build_polygon;
+    private JButton adjust_vertex;
+    private JButton rotate_polygon;
+    private JButton stop_button;
+    private JButton reset_button;
+    private JButton quit_button;
+
+    // Menu for attributes
+    private JMenuBar polygon_attributes;
+    private JMenu menu;
+    private JRadioButtonMenuItem white, red, orange, pink, green;
+    private JCheckBoxMenuItem polygon_mode, bspline_mode;
+    private JCheckBoxMenuItem filled;
+
+    // Booleans to tell if the polygon has been built, if the user
+    // is currently dragging, and if the user can drag a vertex
+    private boolean polygon_built;
+    private boolean adjusting_vertex;
+    private boolean dragging;
+
+    // Array of possible colors
+    Color colors[];
+
+    // Polygon that will be built
+    private Polygon user_poly;
+    private Polygon drag_poly;
+
+    // Timer elements
+    Timer rotate_time;
+    private int angle;
+    private boolean timer_running;
+
+    // Booleans for bspline and filed polygon modes
+    private boolean bspline;
+    private boolean filled_mode;
+
+    // Index of dragging vertex
+    private int drag_index;
+
+    // Constructor
+    polygon_driver(){
+        super("Polygon");
+        // Build the GUI
+        buildGUI();
+
+        // Initialize variables
+        init();
+
+        // Make menu bar
+        make_menu();
+    }
+
+    private void buildGUI(){
+        // Set up the container
+        frame = new JFrame();
+        c = new Container();
+        c = frame.getContentPane();
+
+        // Set up the panels
+        draw_panel = new JPanel();
+        draw_panel.setBackground(Color.BLACK);
+        controls_panel = new JPanel();
+
+        // Set up buttons
+        build_polygon = new JButton("Build");
+        build_polygon.addActionListener(this);
+        adjust_vertex = new JButton("Adjust Vertex");
+        adjust_vertex.addActionListener(this);
+        rotate_polygon = new JButton("Rotate");
+        rotate_polygon.addActionListener(this);
+        stop_button = new JButton("Stop");
+        stop_button.addActionListener(this);
+        reset_button = new JButton("Reset");
+        reset_button.addActionListener(this);
+        quit_button = new JButton("Quit");
+        quit_button.addActionListener(this);
+
+        // Add buttons to control panel
+        controls_panel.add(build_polygon);
+        controls_panel.add(adjust_vertex);
+        controls_panel.add(rotate_polygon);
+        controls_panel.add(stop_button);
+        controls_panel.add(reset_button);
+        controls_panel.add(quit_button);
+
+        // Add mouse listener
+        draw_panel.addMouseListener(this);
+
+        // Add panels to the container
+        c.add(controls_panel, BorderLayout.NORTH);
+        c.add(draw_panel, BorderLayout.CENTER);
+
+        // Make everything visible
+        frame.setSize(800,800);
+        frame.setVisible(true);
+    }
+
+    private void init(){
+        // Set animating, polygon built, and adjusting a vertex to false
+        polygon_built = false;
+        adjusting_vertex = false;
+        dragging = false;
+
+        // Make polygon class
+        user_poly = new Polygon();
+
+        // Set colors
+        colors = new Color[]{Color.white, Color.RED, Color.orange, Color.pink, Color.green};
+
+        // Index of the vertex that is being drawn
+        drag_index = 0;
+
+        // Set the inital angle of rotation
+        angle = 45;
+
+        // The timer is not running by default
+        timer_running = false;
+
+        // Set filled and bspline to false
+        bspline = false;
+        filled_mode = false;
+    }
+
+    // Make menu
+    public void make_menu(){
+        // Create menu bar
+        polygon_attributes = new JMenuBar();
+
+        // Build the menu
+        menu = new JMenu("Attributes");
+        polygon_attributes.add(menu);
+
+        // Radio buttons for color options
+        ButtonGroup color_group = new ButtonGroup();
+        white = new JRadioButtonMenuItem("White");
+        red = new JRadioButtonMenuItem("Red");
+        orange = new JRadioButtonMenuItem("Orange");
+        pink = new JRadioButtonMenuItem("Pink");
+        green = new JRadioButtonMenuItem("Green");
+
+        // Set white to default and add buttons to color group
+        white.setSelected(true);
+        color_group.add(white);
+        color_group.add(red);
+        color_group.add(orange);
+        color_group.add(pink);
+        color_group.add(green);
+
+        // Add radio buttons to menu and add seperator
+        menu.add(white);
+        menu.add(red);
+        menu.add(orange);
+        menu.add(pink);
+        menu.add(green);
+        menu.addSeparator();
+
+        // Make check boxes
+        polygon_mode = new JCheckBoxMenuItem("Polygon Mode");
+        polygon_mode.setSelected(true);
+        bspline_mode = new JCheckBoxMenuItem("B-spline Mode");
+        menu.add(polygon_mode);
+        menu.add(bspline_mode);
+        menu.addSeparator();
+        filled = new JCheckBoxMenuItem("Fill polygon?");
+        menu.add(filled);
+
+        // Add item listeners
+        white.addItemListener(this);
+        red.addItemListener(this);
+        orange.addItemListener( this);
+        pink.addItemListener(this);
+        green.addItemListener( this);
+        polygon_mode.addItemListener( this);
+        bspline_mode.addItemListener(this);
+        filled.addItemListener( this);
+
+        // Add menu to frame
+        frame.setJMenuBar(polygon_attributes);
+
+    }
+
+    public void itemStateChanged(ItemEvent e){
+        // Check the item event
+        if(e.getSource() == white){
+            polygon_panel.set_color(colors[0]);
+        }
+        if(e.getSource() == red){
+            polygon_panel.set_color(colors[1]);
+        }
+        if(e.getSource() == orange){
+            polygon_panel.set_color(colors[2]);
+        }
+        if(e.getSource() == pink){
+            polygon_panel.set_color(colors[3]);
+        }
+        if(e.getSource() == green){
+            polygon_panel.set_color(colors[4]);
+        }
+        if(e.getSource() == filled){
+            polygon_panel.set_filled();
+            filled_mode = !filled_mode;
+        }
+        if(e.getSource() == bspline_mode){
+            polygon_panel.set_bspline();
+            bspline = !bspline;
+        }
+        if(e.getSource() == polygon_mode){
+            polygon_panel.set_polygon_mode();
+        }
+        c.revalidate();
+        c.repaint();
+    }
+
+    private void displayVerticesPopup(){
+        // Make popup
+        JPopupMenu popup = new JPopupMenu();
+        popup.add("You must make at least 4 vertices, you have made: " + user_poly.npoints);
+        popup.setPopupSize(400,200);
+
+        // Show the popup
+        popup.show(c, 400, 400);
+
+    }
+
+    // Handle different actions
+    public void actionPerformed(ActionEvent e){
+        // Get text of the button clicked
+        String input_text = e.getActionCommand();
+
+        // Quit the app and clear up memory
+        if(input_text.equals("Quit")){
+            System.gc();
+            System.exit(0);
+        }
+
+        // If the polygon has been built, allow for buttons to be clicked
+        if(polygon_built) {
+            // Adjust vertex
+            if (input_text.equals("Adjust Vertex")) {
+                // If not adjusting, begin adjusting vertex
+                if(!adjusting_vertex) {
+                    adjusting_vertex = true;
+                    adjust_vertex.setBackground(Color.red);
+                }
+                else{
+                    adjusting_vertex = false;
+                    adjust_vertex.setBackground(null);
+                }
+                c.repaint();
+            }
+            // Rotate polygon
+            else if(input_text.equals("Rotate")){
+                // Change button color to indicate the polygon is rotating
+                rotate_polygon.setBackground(Color.red);
+                c.repaint();
+
+                // Create timer, runs every 2 seconds
+                rotate_time = new Timer(2000, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        // Set the angle and increment by 45 degrees
+                        polygon_panel.set_angle(angle);
+                        angle += 45;
+                        polygon_panel.set_drag_box(false);
+
+                        // Once the polygon has rotated 360 degrees, stop the timer
+                        if(angle > 360){
+                            Timer stop = (Timer)actionEvent.getSource();
+                            stop.stop();
+                            angle = 0;
+                            timer_running = false;
+                            rotate_polygon.setBackground(null);
+                            polygon_panel.new_polygon(user_poly, bspline, filled_mode, true);
+                            c.repaint();
+                        }
+                    }
+                });
+                rotate_time.start();
+                timer_running = true;
+                c.repaint();
+            }
+            // Change polygon attributes
+            else if(input_text.equals("Attributes")){
+
+            }
+            // Stop polygon rotation
+            else if(input_text.equals("Stop")){
+                // If the timer is running, stop the timer
+                if(timer_running) {
+                    rotate_time.stop();
+                    rotate_polygon.setBackground(null);
+                }
+            }
+            // Reset polygon to allow for user to build new one
+            else if(input_text.equals("Reset")){
+                // Set polygon built to false
+                polygon_built = false;
+                bspline = false;
+                filled_mode = false;
+
+                // Reset attributes menu
+                make_menu();
+
+                // Reset the panel and polygon
+                c.remove(polygon_panel);
+                c.add(draw_panel);
+                c.repaint();
+                user_poly.reset();
+            }
+        }
+        else{
+            // Build the polygon
+            if(input_text.equals("Build")){
+                // If the number of vertices is greater than 3, build the polygon
+                if(user_poly.npoints > 3) {
+                    // Set polygon built to true
+                    polygon_built = true;
+
+                    // Render the polygon panel
+                    polygon_panel = new polyPanel(user_poly);
+
+                    // Set the polygon panels background to black and add mouse listener
+                    polygon_panel.setBackground(Color.black);
+                    polygon_panel.addMouseListener(this);
+                    polygon_panel.addMouseMotionListener(this);
+
+                    // Replace the panel with the polygon panel
+                    c.remove(draw_panel);
+                    c.add(polygon_panel, BorderLayout.CENTER);
+
+                    // Redraw screen
+                    c.revalidate();
+                    c.repaint();
+
+                }
+                // If the user hasn't clicked enough, make a popup telling them to click 3 times
+                else{
+                    displayVerticesPopup();
+                }
+            }
+        }
+    }
+
+
+    // Handle mouse clicks
+    public void mousePressed(MouseEvent e){
+         // Get the current point clicked
+        Point curr_point = e.getPoint();
+
+        // Create new vertex if polygon hasn't been built yet
+        if(!polygon_built){
+            // Get the clicks coordinates
+            int x = curr_point.x;
+            int y = curr_point.y;
+
+            // Add the point to the polygon
+            user_poly.addPoint(x, y);
+        }
+
+        // If polygon is built and user is dragging
+        else if(adjusting_vertex && !dragging){
+                if (polygon_panel.clickInRect(curr_point) != -1) {
+                    dragging = true;
+                    drag_index = polygon_panel.clickInRect(curr_point);
+                }
+        }
+    }
+
+    public void mouseDragged(MouseEvent e){
+        if(adjusting_vertex){
+            // Redraw the vertex that is being drawn
+            if(dragging){
+                int[] x = user_poly.xpoints;
+                int[] y = user_poly.ypoints;
+                x[drag_index] = e.getX();
+                y[drag_index] = e.getY();
+                drag_poly = new Polygon(x,y,x.length);
+                polygon_panel.new_polygon2(drag_poly);
+                c.repaint();
+            }
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+
+    public void mouseReleased(MouseEvent e)
+    {
+        // If the polygon is being built
+        if(dragging) {
+            dragging = false;
+            polygon_panel.new_polygon(drag_poly, bspline, filled_mode, true);
+            c.repaint();
+        }
+    }
+    public void mouseExited(MouseEvent e)
+    {
+
+    }
+    public void mouseEntered(MouseEvent e)
+    {
+
+    }
+    public void mouseClicked(MouseEvent e)
+    {
+
+    }
+
+    public static void main(String args[]){
+        // Make application
+        polygon_driver app = new polygon_driver();
+
+        // Add window listener
+        app.addWindowListener(
+                new WindowAdapter(){
+                    public void windowClosing(WindowEvent e)
+                    {
+                        System.exit(0);
+                    }
+                }
+        );
+    }
+}
